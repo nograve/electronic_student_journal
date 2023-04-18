a2c1f619c08badb530aec46150ccff62667bf786
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-04-18 22:51:14 +0300

    Add Ukrainian l10n

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Adryan Eka Vandra
-// https://github.com/adryanev/flutter-electronic_student_journal-architecture-electronic_student_journal
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'dart:async';
 import 'dart:developer';
 
