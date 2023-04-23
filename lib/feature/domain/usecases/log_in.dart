c9acf318c2af4572892d457671a4eab5b17e31c5
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-04-23 10:55:03 +0300

    Sort imports

--- lib/feature/domain/usecases/log_in.dart
+++ lib/feature/domain/usecases/log_in.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:electronic_student_journal/core/error/failure.dart';
+import 'package:electronic_student_journal/core/usecase/usecase.dart';
 import 'package:electronic_student_journal/feature/domain/entities/user_entity.dart';
 import 'package:electronic_student_journal/feature/domain/repositories/auth_repository.dart';
-import 'package:electronic_student_journal/core/usecase/usecase.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'log_in.freezed.dart';
