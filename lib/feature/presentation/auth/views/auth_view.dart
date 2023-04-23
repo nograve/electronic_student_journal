afe79e7cd33e68c06b43823e3ccc5ac7d858ab56
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-04-23 09:24:14 +0300

    Move gen to lib

--- lib/feature/presentation/auth/views/auth_view.dart
+++ lib/feature/presentation/auth/views/auth_view.dart
@@ -1,4 +1,4 @@
-import 'package:electronic_student_journal/core/gen/assets.gen.dart';
+import 'package:electronic_student_journal/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
