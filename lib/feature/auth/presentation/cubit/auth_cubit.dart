89d4b639cbd32a2e11e7a0d0986c422205e0f5e6
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-04-29 09:32:31 +0300

    Use logger

--- lib/feature/auth/presentation/cubit/auth_cubit.dart
+++ lib/feature/auth/presentation/cubit/auth_cubit.dart
@@ -2,6 +2,7 @@ import 'package:electronic_student_journal/feature/auth/domain/entities/user_ent
 import 'package:electronic_student_journal/feature/auth/domain/usecases/post_sign_in.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:logger/logger.dart';
 
 part 'auth_cubit.freezed.dart';
 part 'auth_state.dart';
@@ -12,6 +13,7 @@ class AuthCubit extends Cubit<AuthState> {
         super(const _Loading());
 
   final PostSignIn _postSignIn;
+  final logger = Logger();
 
   Future<void> signIn(SignInParams params) async {
     emit(const AuthState.loading());
@@ -20,7 +22,7 @@ class AuthCubit extends Cubit<AuthState> {
     response.fold(
       (failure) => emit(_Failure(failure.message)),
       (userEntity) {
-        print(userEntity);
+        logger.i(userEntity.toString());
         emit(_Success(userEntity));
       },
     );
