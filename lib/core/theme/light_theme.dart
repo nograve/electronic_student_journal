ae91b0f5bd551976b5c84a514ccef6bc42e10f9c
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-05-27 12:40:13 +0300

    Add roboto mono font globally

--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme = ThemeData(
+  textTheme: GoogleFonts.robotoMonoTextTheme(),
   inputDecorationTheme: const InputDecorationTheme(
     border: OutlineInputBorder(borderSide: BorderSide(width: 3)),
   ),
