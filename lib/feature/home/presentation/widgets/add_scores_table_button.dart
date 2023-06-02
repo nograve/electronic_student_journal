0ee4a3a93af68bf25d479355c6998f78429255f7
Ihor Zakharov <Zakharov.Ih.V@nmu.one>
2023-06-02 19:33:03 +0300

    Design add table button

--- lib/feature/home/presentation/widgets/add_scores_table_button.dart
+++ lib/feature/home/presentation/widgets/add_scores_table_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AddScoresTableButton extends StatelessWidget {
   const AddScoresTableButton({super.key});
@@ -7,7 +8,22 @@ class AddScoresTableButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {},
-      child: const Icon(Icons.add),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.add,
+            size: 96.r,
+          ),
+          // Text(
+          //   'Add',
+          //   style: TextStyle(
+          //     fontSize: 16.sp,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // )
+        ],
+      ),
     );
   }
 }
